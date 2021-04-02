cbe2f27ed01a4d067fc0b63d0f635d075201b7e1
Nicole C <nclotus11@gmail.com>
2021-04-02 19:59:47 -0400

    commit -m "rename data & write path to csv

---
+++ PyBank/main.py
@@ -0,0 +1,19 @@
+#modules
+import os
+import csv
+
+#set path for file
+csvpath = os.path.join("Resources", "budget_data.csv")
+print(csvpath)
+
+#lists to store data
+Months = []
+
+#open budget data as csvfile
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    for row in csvreader:
+        #Total number of months included in the dataset
+        TotalMonths = len(Months(csvreader))
+        
+print(TotalMonths)
