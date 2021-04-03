5e7769011962df682a313fd523487937a65c76c3
Nicole C <nclotus11@gmail.com>
2021-04-02 20:51:58 -0400

    commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,3 +9,7 @@ print(csvpath)
 #open budget data as csvfile
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+
+    #read header row
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
