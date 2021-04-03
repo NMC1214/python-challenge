5737a4441afa6b65806fd7dc30d5c4eed4d21edb
Nicole C <nclotus11@gmail.com>
2021-04-03 16:31:38 -0400

    commit -m "Progress to convert str to float"

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,15 +7,47 @@ csvpath = os.path.join("PyBank","Resources","budget_data.csv")
 print(csvpath)
 
 #open budget data as csvfile
-with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+with open(csvpath, 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
 
-    #read header row
-    csv_header = next(csvreader)
+    #Skip header row
+    next(csv_reader)
 
-    #Count Number Months
-    Months = list(csvreader)
-    Total_Months = len(Months)
-    print(Total_Months)
+    #Create list of Profit/Losses Values
+    Sum_Profit = []
+    for line in csv_reader:
+ 
+        Sum_Profit.append(line[1])
+        Profit_floats = [float(item) for item in Sum_Profit]
 
-    #Sum of "profit/loses" over the entire period
+    #Print first profit value
+    print(Profit_floats[0])
+
+    #print last profit value
+    print(Profit_floats[85])
+
+    print(type(Profit_floats[1]))
+
+    print("Total Months: ",len(Profit_floats))
+
+    #net total amount of "Profit/Losses" over the entire period
+    Sum_Values = sum(Profit_floats)
+    print("Total: ",Sum_Values)
+
+    #return max value
+    Largest_Profit = max(Profit_floats)
+    print("Largest Profit Value: ",Largest_Profit)
+
+    #return smallest value
+    Largest_Loss = min(Profit_floats)
+    print("Biggest Loss: ",Largest_Loss)
+
+
+
+
+
+            
+
+
+
+    
