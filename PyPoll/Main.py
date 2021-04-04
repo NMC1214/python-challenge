fb50b438c4a15bd1676575141cd908c0c5c8e1ce
Nicole C <nclotus11@gmail.com>
2021-04-04 14:30:20 -0400

    commit -m "Most recent work"

--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -9,6 +9,13 @@ print(csvpath)
 #open budget data as csvfile
 with open(csvpath, 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
-    
+
+    next(csv_reader)
+
+    Elect_dict = {"Voter ID":}
+
+    for rows in csv_reader:
+       
+        
 
 
