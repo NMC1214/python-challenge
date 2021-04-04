de688cef1d8d654c0793d7831fa2794f96dcc6db
Nicole C <nclotus11@gmail.com>
2021-04-04 15:09:37 -0400

    commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,36 +9,53 @@ csvpath = os.path.join("PyBank","Resources","budget_data.csv")
 with open(csvpath) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
 
+    #Skip header line
     csv_header = next(csv_reader)
 
+    #create lists for data
     Months = []
     Profit = []
     Profit_Change = []
 
+    #loop for each row in csvfile
     for row in csv_reader:
 
+        #add first column to Month list
         Months.append(row[0])
 
+        #add profit/loss to pofit list
         Profit.append(int(row[1]))
 
+    #number items in months list
     Total_Months = len(Months)
     print("Total Months: ", Total_Months)
 
+    #sum of profit list
     Total_Profit = sum(Profit)
     print("Total: $", Total_Profit)
 
+    #loop for profit list
     for i in range(len(Profit)-1):
+
+        #subtract next item in list from current item
         Profit_Change.append(Profit[i+1]-Profit[i])
 
+    #divide total of profit change list by items in the list to tell average
     Average_Change = sum(Profit_Change) / len(Profit_Change)
-
     print("Average Change: $", Average_Change)
 
+    #find largest change in list
     Great_Inc = max(Profit_Change)
-    print("Greatest Increase in Profits: ", "($",Great_Inc,")")
+    #find index of the item in list with largest change
+    max_Inc_index = Profit_Change.index(Great_Inc)
+    print("Greatest Increase in Profits: ", Months[(max_Inc_index + 1)], "($",Great_Inc,")")
 
+    #find smallet item in change list
     Great_Loss = min(Profit_Change)
-    print("Greatest Decrease in Profits: ", "($", Great_Loss, ")")
+    #find index of that item
+    max_Dec_index = Profit_Change.index(Great_Loss)
+    print("Greatest Decrease in Profits: ", Months[(max_Dec_index + 1)], "($", Great_Loss, ")")
+
         
 
 
