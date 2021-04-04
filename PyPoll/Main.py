e7c7eba7382171688cdfe0fbb883a022c4203f6f
Nicole C <nclotus11@gmail.com>
2021-04-04 16:10:18 -0400

    commit

--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -7,15 +7,44 @@ csvpath = os.path.join("PyPoll","Resources","election_data.csv")
 print(csvpath)
 
 #open budget data as csvfile
-with open(csvpath, 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
+with open(csvpath) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
 
-    next(csv_reader)
+    #Skip header line
+    csv_header = next(csv_reader)
 
-    Elect_dict = {"Voter ID":}
+    #create lists of data
+    Voter_ID = []
+    County = []
+    Candidate = []
+    Khan_Votes = []
+    Correy_Votes = []
+    Li_Votes = []
+    OTool_Votes = []
 
-    for rows in csv_reader:
-       
+    #loop for each row in csv file
+    for row in csv_reader:
+
+        #add values into lists by index
+        Voter_ID.append(row[0])
+        County.append(row[1])
+        Candidate.append(row[2]) 
+
+    Num_Khan = Candidate.count("Khan")
+    print(Num_Khan)
+
+    Total_Votes = len(Voter_ID)
+    print("Total Votes: ", Total_Votes)
+
+
+    Unique_Candidate = []
+
+    for x in Candidate:
+        if x not in Unique_Candidate:
+                Unique_Candidate.append(x)
+
+    print(Unique_Candidate)
+
         
-
-
+    
+    
